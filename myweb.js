f9d2913413270f9726a72b62ab5cc4aedea7424a
chandan bundekar <86016241+hjdcuyd8yd@users.noreply.github.com>
2023-02-08 21:42:33 +0530

    Add files via upload

---
+++ myweb.js
@@ -0,0 +1,9 @@
+hamburger = document.querySelector(".hamburger");
+resp = document.querySelector(".resp");
+ulresp = document.querySelector(".ulresp");
+cross = document.querySelector(".cross");
+
+hamburger.addEventListener("click", () => {
+    resp.classList.toggle("ulresp");
+    li.classList.toggle("resp");
+});
